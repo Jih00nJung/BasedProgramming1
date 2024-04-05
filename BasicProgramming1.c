b7545ee3013bd032906c2b29e6e159ce72f0e9c2
Jih00nJung <wjdwlgns11q@gmail.com>
2024-04-05 14:12:07 +0900

    수업별 함수 분류
    ps. 아예 파일별로 나누고 싶은데 그건 좀 복잡해서 아직 보류..

--- BasicProgramming1.c
+++ BasicProgramming1.c
@@ -4,15 +4,15 @@
 
 /* 주석문: 실행시 무시하고 실행됨 */
 
-int main(void) {
-/* ----------------------------------------------------------------
+int class1(void) {
+    printf("Hello, world\n");
+    printf("반갑습니다.\n");
 
-//  강의 1주차
-  printf("Hello, world\n");
-  printf("반갑습니다.\n");
+    return 0;
+}
 
 
-//  강의 2주차
+int class2(void) {
     printf("삑~삑~삑 소리 세 번 : \a\a\a ♬ ♬ ♬\n");
     printf("테스트 글자\r 앞에 있는 글을 덮어씁니다.\n");
     printf("[Tab] 키 사용\t 일정한 간격\t 좋습니다.\n");
@@ -21,13 +21,9 @@ int main(void) {
 //  표준 출력 형식 지정자
     printf("%d, %d\n", 123, 456789);
 
-
-
     printf("|%d|\n", 789);
     printf("|%5d|\n", 789);
     printf("|%05d|\n", 789);
-
-
 
 //  C 언어에서는 String을 사용하는 자바와 달리 char 변수명[]을 사용한다.
     char name[20];
@@ -40,9 +36,6 @@ int main(void) {
 
     printf("안녕하세요. %s님\n", name);
 
-*/ // -----------------------------------------------------------------
-
-/*
 //  정수 입력 받아서 출력하기
     int num, num2, plus;
     printf("정수를 입력하세요.");
@@ -53,16 +46,14 @@ int main(void) {
     plus = num + num2;
 
     printf("입력된 정수의 합은 %d + %d = %d입니다.\n", num, num2, plus);
-*/
+
 
 //  직접형 표준 입출력 함수 : getch() : enter를 누르지 않고도 값이 전달되는 함수, 값이 전달되는 시점이 어떻게 되는 걸까? --- keyup이 발생될때 값이 전달된다고 한다.
 
-    /*char ch;
+    char ch;
     printf("문자를 입력하세요.");
     ch = getchar();
     printf("%c", ch);
-*/
-/*
     char op1, op2;
 
     printf("문자 2개를 연속해서 입력하세요.\n");
@@ -77,8 +68,6 @@ int main(void) {
     printf("첫번째 문자 출력 : %c\n", op1);
     printf("두번째 문자 출력 : %c\n", op2);
 
-*/
-
 //  도전 문제 2
     float su;
 
@@ -88,11 +77,18 @@ int main(void) {
     printf("소수 둘째 자리까지 출력 : %.2f\n", su);
     printf("전체 자리수 6자리, 소수 첫째자리까지 출력 : %06.1f\n", su);
 
+    return 0;
+}
+
+
+int class3(void) {
+
+    return 0;
+}
 
 
 
-
-
-
+int main(void) {
+    class3();
     return 0;
 }
--- class2.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main2(void) {
-    printf("반갑습니다.");
-
-    return 0;
-}
